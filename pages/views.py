b169a1461574b3201feb93a290f9d3c55ab84e61
James Parker <parkerman23@gmail.com>
2023-01-14 03:24:07 +0000

    First

---
+++ pages/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+
+
+
+
+def home(request):
+	return render(request, "home.html", {})
+
+def about(request):
+	from pages.namer import namer
+	return render(request, "about.html", {"my_stuff":namer })
+
+def contact(request):
+	return render(request, "contact.html", {})
+
+def videos(request):
+	return render(request, "videos.html", {})
+
+def gigs(request):
+	return render(request, "gigs.html", {})
